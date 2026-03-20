dc0dce1b4cb4d351692c835da41c206c43396a05
c0rv1n0 <isaaccrowgeneral@gmail.com>
2026-03-20 11:43:57 +0300

    feat: handle text messages with echo reply

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start((ctx) => ctx.reply('Привет! Я - Media-bot. Скинь мне ссылку.'))
 
+bot.on('text', (ctx) => {
+    const text = ctx.message.text
+    ctx.reply('Ты написал: ' + text)
+})
+
 bot.launch()
 console.log('Бот запущен')
