a3537fae00fe539308aea6f9ff4bfa206a608c0e
c0rv1n0 <isaaccrowgeneral@gmail.com>
2026-03-23 14:09:05 +0300

    feat: store links as JSON with tags/person/date fields.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const { Telegraf } = require('telegraf')
 const fs = require('fs')
-const { text } = require('stream/consumers')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
