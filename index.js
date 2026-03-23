f6cdbf5e99f721190b4cadc259047a5de4d0cd6c
c0rv1n0 <isaaccrowgeneral@gmail.com>
2026-03-23 12:31:07 +0300

    feat: store links as JSON with tags/person/date fields.

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 require('dotenv').config()
 const { Telegraf } = require('telegraf')
 const fs = require('fs')
+const { text } = require('stream/consumers')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start((ctx) => ctx.reply('Привет! Я - Media-bot. Скинь мне ссылку.'))
 
 bot.command('list', (ctx) => {
-    fs.readFile('links.txt', 'utf8', (err, data) => {
+    fs.readFile('links.json', 'utf8', (err, data) => {
         if (err) {
             ctx.reply('Список пуст.')
         } else {
-            ctx.reply(data)
+            const links = JSON.parse(data)
+            const text = links.map((l, i) => `${i+1}. ${l.url}`).join('\n')
+            ctx.reply(text)
         }
     })
 })
 bot.on('text', (ctx) => {
     const text = ctx.message.text
 
-    if (text.startsWith('http://') || text.startsWith('https://')){
-        fs.appendFile('links.txt', text + '\n', (err) => {
-            if (err) {
-                ctx.reply('Ошибка при сохранении.')
-            } else {
-                ctx.reply('Ссылка сохранена.')
-            }
+    if (text.startsWith('http://') || text.startsWith('https://')) {
+
+        const newLink = {
+            url: text,
+            tags: [],
+            person: '',
+            date: new Date().toISOString().slice(0, 10)
+        }
+
+        fs.readFile('links.json', 'utf8', (err, data) => {
+            const links = err ? [] : JSON.parse(data)
+            links.push(newLink)
+
+            fs.writeFile('links.json', JSON.stringify(links, null, 2), (err) =>     {
+                if (err) {
+                    ctx.reply('Ошибка при сохранении.')
+                } else {
+                    ctx.reply('Ссылка сохранена.')
+                }
+            })
         })
         ctx.reply('Вижу ссылку: ' + text)
     } else {
